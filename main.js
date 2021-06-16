2d1ac4042e4d41325d90448e071a9b3b46500b68
Roald Ong <roald.ong@gmail.com>
2021-06-17 00:09:44 +0800

    first working version with rclnodejs and electron

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*import electron module and store it inside two local constant object variables app and BrowserWindow*/
 const { app, BrowserWindow } = require('electron')
+const rcl_main = require('rclnodejs')
 
 
 function createWindow () {
@@ -14,4 +15,12 @@ function createWindow () {
 
 app.whenReady().then(() => {
   createWindow()
+
+const rclnodejs = require('rclnodejs');
+rclnodejs.init().then(() => {
+  const node = new rclnodejs.Node('publisher_example_node');
+  const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
+  publisher.publish(`Hello ROS 2 from rclnodejs`);
+  node.spin();
+});
 })
