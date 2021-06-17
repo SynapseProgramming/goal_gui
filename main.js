b709cb07b5c9a54b5d847fb56d5aeb3bb8b721e9
Roald Ong <roald.ong@gmail.com>
2021-06-17 10:54:16 +0800

    Added in preloaded scripts.

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 /*import electron module and store it inside two local constant object variables app and BrowserWindow*/
 const { app, BrowserWindow } = require('electron')
 const rcl_main = require('rclnodejs')
-
+const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 1920,
-    height: 1080
+    height: 1080,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
@@ -23,4 +26,8 @@ rclnodejs.init().then(() => {
   publisher.publish(`Hello ROS 2 from rclnodejs`);
   node.spin();
 });
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
