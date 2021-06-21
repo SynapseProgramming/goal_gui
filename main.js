dae1e2497a505c836189c29ff19e31a5e3b1d4d6
Roald Ong <roald.ong@gmail.com>
2021-06-21 10:50:03 +0800

    Added in menu button that when pressed, would send a string to ros2.

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 /*import electron module and store it inside two local constant object variables app and BrowserWindow*/
 //get these sub modules from electron
-const { app, BrowserWindow, Menu } = require('electron')
-const rcl_main = require('rclnodejs')
+const {
+  app,
+  BrowserWindow,
+  Menu
+} = require('electron')
+const rclnodejs = require('rclnodejs')
 const path = require('path')
 
-function createWindow () {
+var node
+var publisher
+var count = 0
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 1920,
     height: 1080,
@@ -16,41 +24,34 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
-const menu_template=[{
+const menu_template = [{
   label: 'menu 1',
   submenu: [{
     label: 'hehe',
     click: async () => {
-            //ros stuff here
-          /*      rclnodejs.init().then(() => {
-                  const node = new rclnodejs.Node('publisher_example_node');
-                  const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
-                  publisher.publish(`Hello ROS 2 from rclnodejs`);
-                  node.spin();
-                }); */
-             const { shell } = require('electron')
-                      await shell.openExternal('https://electronjs.org')          }
+      var string_count = String(count)
+      publisher.publish(string_count)
+      count = count + 1
+    }
   }]
 }]
 
 
+rclnodejs.init().then(() => {
+  node = new rclnodejs.Node('publisher_example_node');
+  publisher = node.createPublisher('std_msgs/msg/String', 'topic');
+  node.spin();
+});
+
 
 app.whenReady().then(() => {
   createWindow()
- const menu=Menu.buildFromTemplate(menu_template)
- Menu.setApplicationMenu(menu)
-}
-/*
-rclnodejs.init().then(() => {
-  const node = new rclnodejs.Node('publisher_example_node');
-  const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
-  publisher.publish(`Hello ROS 2 from rclnodejs`);
-  node.spin();
-});
-}
-*/
-)
 
-app.on('window-all-closed', function () {
+  const menu = Menu.buildFromTemplate(menu_template)
+  Menu.setApplicationMenu(menu)
+
+})
+
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') app.quit()
 })
