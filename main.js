f05154c43526f4e873f9545985f6f6df7f247cbe
Roald Ong <roald.ong@gmail.com>
2021-06-21 09:37:10 +0800

    Added in a menu with a callback function.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*import electron module and store it inside two local constant object variables app and BrowserWindow*/
-const { app, BrowserWindow } = require('electron')
+//get these sub modules from electron
+const { app, BrowserWindow, Menu } = require('electron')
 const rcl_main = require('rclnodejs')
 const path = require('path')
 
@@ -15,18 +16,40 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
+const menu_template=[{
+  label: 'menu 1',
+  submenu: [{
+    label: 'hehe',
+    click: async () => {
+            //ros stuff here
+          /*      rclnodejs.init().then(() => {
+                  const node = new rclnodejs.Node('publisher_example_node');
+                  const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
+                  publisher.publish(`Hello ROS 2 from rclnodejs`);
+                  node.spin();
+                }); */
+             const { shell } = require('electron')
+                      await shell.openExternal('https://electronjs.org')          }
+  }]
+}]
+
+
 
 app.whenReady().then(() => {
   createWindow()
-
-const rclnodejs = require('rclnodejs');
+ const menu=Menu.buildFromTemplate(menu_template)
+ Menu.setApplicationMenu(menu)
+}
+/*
 rclnodejs.init().then(() => {
   const node = new rclnodejs.Node('publisher_example_node');
   const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
   publisher.publish(`Hello ROS 2 from rclnodejs`);
   node.spin();
 });
-})
+}
+*/
+)
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
