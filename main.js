3f1ad9092b353345e8350e7e6818287f3a0616c6
Roald Ong <roald.ong@gmail.com>
2021-07-26 14:05:33 +0800

    added in library implementation for rclnodejs and roslib

--- main.js
+++ main.js
@@ -2,19 +2,24 @@
 const {
   app,
   BrowserWindow
-} = require('electron')
-const path = require('path')
+} = require('electron');
+const path = require('path');
+//const  ROSLIB = require('roslib');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1920,
-    height: 1080
-  })
+    height: 1080,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
+
 })
